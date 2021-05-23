9064f7e2a6be126487c44148486b954c857784bd
Matthew Grosman <grosmanmatt@gmail.com>
2021-05-22 22:43:18 -0700

    Updated pom.xml

--- src/IndexServlet.java
+++ src/IndexServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 // server endpoint URL
 @WebServlet("/index-servlet")
