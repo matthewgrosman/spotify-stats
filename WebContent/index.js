4830c4dd2e9a1d2325930dae3b982469c2eb42d4
Matthew Grosman <grosmanmatt@gmail.com>
2021-05-23 01:16:07 -0700

    Fixed comment spacing

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,8 +1,8 @@
 /**
-* This is the JavaScript file that displays the user's top artists, songs and albums. This
-* is done through making a jQuery.ajax call to the Java servlet in the backend of the
-* program, parsing the returned data, and then using jQuery to grab the HTML elements and
-* append to them the appropriate data.
+ * This is the JavaScript file that displays the user's top artists, songs and albums. This
+ * is done through making a jQuery.ajax call to the Java servlet in the backend of the
+ * program, parsing the returned data, and then using jQuery to grab the HTML elements and
+ * append to them the appropriate data.
  */
 
 /**
@@ -12,7 +12,6 @@
 function handleResponse(data) {
     console.log(JSON.parse(JSON.stringify(data)));
 }
-
 
 /*
 Makes an HTTP request to the backend Java servlet. Upon success, the returned JSON data is sent
