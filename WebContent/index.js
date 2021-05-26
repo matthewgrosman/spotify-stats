f2013bc7933743c39634a217c0c552681f812b4e
Matthew Grosman <grosmanmatt@gmail.com>
2021-05-25 22:13:16 -0700

    Began working on authorization code

--- WebContent/index.js
+++ WebContent/index.js
@@ -6,24 +6,70 @@
  */
 
 /**
- * Handles the response
- * @param data  some json data
+ * Given a parameter name, this function returns the value of that parameter in the URL.
+ * For example, the url "https://www.somewebsite.com/?param1=hello", calling
+ * getParameterByName("param1") would return "hello". When called with a parameter that
+ * does not exist, this function returns null.
+ *
+ * @param target    a String representing the parameter whose value we want
+ * @returns {string|null}
+ */
+function getParameterByName(target) {
+    // Get request URL
+    let url = window.location.href;
+
+    // Encode target parameter name to url encoding
+    target = target.replace(/[\[\]]/g, "\\$&");
+
+    // Ues regular expression to find matched parameter value
+    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+
+    // Return the decoded parameter value
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+/**
+ * Handles the response from the backend Java servlet.
+ * @param data  The JSON data returned from the backend.
  */
 function handleResponse(data) {
-    console.log(JSON.parse(JSON.stringify(data)));
+    console.log("got here2");
+    let json = JSON.parse(JSON.stringify(data));
+    console.log("got here3");
+    if (json["user_type"] === "new") {
+        // location.href = json["uri"];
+        console.log(json);
+    }
+    else {
+        console.log(json);
+    }
 }
+
 
 /*
 Makes an HTTP request to the backend Java servlet. Upon success, the returned JSON data is sent
-to the function handleResponse.
+to the function handleResponse. Before the call is made, we check to see if we have an access token
+in the url, which is denoted through the "code" parameter. We pass this through in the url.
 */
+let ajaxURL = "statisticsServlet";
+let code = getParameterByName("code");
+
+if (code != null) {
+    ajaxURL += "?code=" + code;
+}
+
+console.log("got here");
+
 jQuery.ajax({
     // Set the return type to JSON.
     dataType: "json",
     // Set the request method to GET.
     method: "GET",
     // Set the request url, which is mapped in the Java servlet.
-    url: "statisticsServlet",
+    url: ajaxURL,
     // If successful, call the handleResponse function, and pass as a parameter the data returned by the call.
     success: (resultData) => handleResponse(resultData)
 });
--- src/StatisticsServlet.java
+++ src/StatisticsServlet.java
@@ -11,28 +11,89 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URI;
+import java.util.concurrent.CompletableFuture;
 
 import com.google.gson.JsonObject;
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.SpotifyHttpManager;
+import com.wrapper.spotify.enums.ModelObjectType;
+import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
+import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
+import com.wrapper.spotify.requests.data.personalization.GetUsersTopArtistsAndTracksRequest;
+import com.wrapper.spotify.requests.data.personalization.interfaces.IArtistTrackModelObject;
 
 @WebServlet(name="StatisticsServlet", urlPatterns="/statisticsServlet")
 public class StatisticsServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		final String clientId = "649c589d7ba94443ae40c6bc10025966";
+		final String clientSecret = "48c7050d60a646f89663acfd14e208b6";
+		final URI redirectURI = SpotifyHttpManager.makeUri( "http://localhost:8080/spotify-stats/");
+
+		String test = "";
+
 		// Create a PrintWriter to write the response back to the front-end
 		PrintWriter out = response.getWriter();
 
 		// Set the response type to JSON
 		response.setContentType("application/json");
+		String accessToken = request.getParameter("code");
 
 		try {
 			JsonObject responseJsonObject = new JsonObject();
-			responseJsonObject.addProperty("test", "URMOM2");
+
+			final SpotifyApi api = SpotifyApi.builder()
+					.setClientId(clientId)
+					.setClientSecret(clientSecret)
+					.setRedirectUri(redirectURI)
+					.build();
+
+			if (accessToken == null || accessToken.equals("")) {
+				final AuthorizationCodeUriRequest authorizationCodeUriRequest = api.authorizationCodeUri()
+						.state("x4xkmn9pu3j6ukrs8n")
+						.scope("user-library-read")
+						.show_dialog(true)
+						.build();
+
+				final CompletableFuture<URI> uriFuture = authorizationCodeUriRequest.executeAsync();
+				final URI uri = uriFuture.join();
+
+				responseJsonObject.addProperty("user_type", "new");
+				responseJsonObject.addProperty("uri", uri.toString());
+				responseJsonObject.addProperty("code", accessToken);
+			}
+			else {
+				final AuthorizationCodeRequest authorizationCodeRequest = api.authorizationCode(accessToken).build();
+				final CompletableFuture<AuthorizationCodeCredentials> authorizationCodeCredentialsFuture = authorizationCodeRequest.executeAsync();
+				final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeCredentialsFuture.join();
+
+				// Set access and refresh token for further "spotifyApi" object usage
+				api.setAccessToken(authorizationCodeCredentials.getAccessToken());
+				api.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+
+				final ModelObjectType type = ModelObjectType.ARTIST;
+
+				final GetUsersTopArtistsAndTracksRequest<? extends IArtistTrackModelObject> getUsersTopArtistsAndTracksRequest = api
+						.getUsersTopArtistsAndTracks(type)
+						.build();
+
+				final CompletableFuture<? extends Paging<? extends IArtistTrackModelObject>> pagingFuture = getUsersTopArtistsAndTracksRequest.executeAsync();
+				final Paging<? extends IArtistTrackModelObject> artistPaging = pagingFuture.join();
+
+				responseJsonObject.addProperty("user_type", "returning");
+				responseJsonObject.addProperty("top", artistPaging.getTotal());
+				responseJsonObject.addProperty("code", authorizationCodeCredentials.getAccessToken());
+			}
+
 
 			out.write(responseJsonObject.toString());
 		}
 		catch (Exception e) {
 			// Write an error message
 			JsonObject jsonObject = new JsonObject();
-			jsonObject.addProperty("errorMessage", e.getMessage());
+			jsonObject.addProperty("errorMessageUpdated2", e.getMessage());
 			out.write(jsonObject.toString());
 
 			// Set response status to 500 (Internal Server Error)
