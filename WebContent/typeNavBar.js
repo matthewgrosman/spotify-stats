d4b663565c7f0779bdc1a5cd8d3a880c4b484bee
Matthew Grosman <grosmanmatt@gmail.com>
2021-06-02 22:12:03 -0700

    Added second nav bar

--- WebContent/typeNavBar.js
+++ WebContent/typeNavBar.js
@@ -1,11 +1,17 @@
-var nav = $('nav');
+var nav = $('#content-type');
+var nav2 = $('#time-range');
 var line = $('<div />').addClass('line');
+var line2 = $('<div />').addClass('line-time');
 
 line.appendTo(nav);
+line2.appendTo(nav2);
 
 var active = nav.find('.active');
+var active2= nav2.find('.active');
 var pos = 0;
+var pos2 = 0;
 var wid = 0;
+var wid2 = 0;
 
 if(active.length) {
     pos = active.position().left;
@@ -13,6 +19,15 @@ if(active.length) {
     line.css({
         left: pos,
         width: wid
+    });
+}
+
+if(active2.length) {
+    pos2 = active2.position().left;
+    wid2 = active2.width();
+    line2.css({
+        left: pos2,
+        width: wid2
     });
 }
 
@@ -57,5 +72,49 @@ nav.find('ul li a').click(function(e) {
 
         pos = position.left;
         wid = width;
+    }
+});
+
+nav2.find('ul li a').click(function(e) {
+    e.preventDefault();
+    if(!$(this).parent().hasClass('active') && !nav2.hasClass('animate')) {
+
+        nav2.addClass('animate');
+
+        var _this = $(this);
+
+        nav2.find('ul li').removeClass('active');
+
+        var position = _this.parent().position();
+        var width = _this.parent().width();
+
+        if(position.left >= pos2) {
+            line2.animate({
+                width: ((position.left - pos2) + width)
+            }, 300, function() {
+                line2.animate({
+                    width: width,
+                    left: position.left
+                }, 150, function() {
+                    nav2.removeClass('animate');
+                });
+                _this.parent().addClass('active');
+            });
+        } else {
+            line2.animate({
+                left: position.left,
+                width: ((pos2 - position.left) + wid2)
+            }, 300, function() {
+                line2.animate({
+                    width: width
+                }, 150, function() {
+                    nav2.removeClass('animate');
+                });
+                _this.parent().addClass('active');
+            });
+        }
+
+        pos2 = position.left;
+        wid2 = width;
     }
 });
