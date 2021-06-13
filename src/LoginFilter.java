173b1cd5e7f9523bca2038b97d665a8f811de486
Matthew Grosman <grosmanmatt@gmail.com>
2021-06-13 01:39:39 -0700

    Trying https fix

--- src/LoginFilter.java
+++ src/LoginFilter.java
@@ -64,7 +64,7 @@ public class LoginFilter implements Filter {
 
         // If this url needs a logged-in user to be accessed, check if user is logged in.
         if (httpRequest.getSession().getAttribute("user_status") == null) {
-            httpResponse.sendRedirect("login.html");
+            httpResponse.sendRedirect("https://spotifstats.com/login.html");
         }
         else {
             chain.doFilter(request, response);
